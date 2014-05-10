44a912eb3faf3c29fac684b49a9b154de4a98962
LorDClockaN <davor@losinj.com>
2014-05-10 17:19:08 +0200

    Fix specific device build
    Build this ONLY for janice

--- display/libcopybit/Android.mk
+++ display/libcopybit/Android.mk
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
+ifeq ($(PRODUCT_DEVICE),janice)
 LOCAL_PATH:= $(call my-dir)
 # HAL module implemenation, not prelinked and stored in
 # hw/<COPYPIX_HARDWARE_MODULE_ID>.<ro.board.platform>.so
@@ -27,3 +27,4 @@ LOCAL_SRC_FILES := copybit.c
 LOCAL_MODULE := copybit.$(TARGET_BOARD_PLATFORM)
 LOCAL_MODULE_TAGS := optional
 include $(BUILD_SHARED_LIBRARY)
+endif
